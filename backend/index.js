13dbf0a4151e51d9b890d9fdc93211c45587d5f1
MandeepSingh-059 <1234mandeep1234@gmail.com>
2021-10-13 22:47:22 +0530

    testing

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+//These are the diffrent endpoints
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+app.get('/login', (req, res) => {
+  res.send('Hello login!')
+})
+app.get('/signup', (req, res) => {
+  res.send('Hello signup!')
 })
 
 app.listen(port, () => {
