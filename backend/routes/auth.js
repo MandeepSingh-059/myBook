bc66e58987fe64e82b2c828edcfd8b5b4bd0ae8b
MandeepSingh-059 <1234mandeep1234@gmail.com>
2021-10-19 00:05:51 +0530

    made a thunderclient collection

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,29 +4,44 @@ const router = express.Router()
 const { body, validationResult } = require('express-validator');
 
 //without router we would do app.get(req, res) here req is request and res is response
-//Create a user using: POST "/api/auth/". Dosent require Authentication
+//Create a user using: POST "/api/auth/createuser". Dosent require Authentication, no login required
 
-router.post("/", [
-    body('name', "Minimum Name Length is 5").isLength({min: 5}),
+router.post("/createuser", [
+    body('name', "Minimum Name Length is 5").isLength({ min: 5 }),
     body('email', "Enter a valid Email").isEmail(),
-    body('password', "Password must be minimum 5 characters long").isLength({min: 5})
-], (req, res) => {
+    body('password', "Password must be minimum 5 characters long").isLength({ min: 5 })
+], async (req, res) => {
     // console.log(req.body)//req ko read kia
     // const user = User(req.body)
-    
+
+    //if error then we return the error
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+        return res.status(400).json({ errors: errors.array() });
     }
 
-    User.create({
-        name: req.body.name,
-        password: req.body.password,
-        email: req.body.email
-    }).then(user => res.json(user))
-    .catch(err=>{console.log(err)
-        res.json({error: "This credential is already registered with us, please enter a diffrent value", message: err.message})
-    })
+    //check if a user with same email already exists 
+    try {
+        let user = await User.findOne({ email: req.body.email })
+        if (user) {
+            return res.status(400).json({ error: "sorry this email is already registered" })
+        }
+        //creating a new user
+        user = await User.create({
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email
+        })
+        res.json(user)
+    } catch (error) {
+        console.error(error.message)
+        resa.status(500).send("Something went wrong")
+    }
+
+    // .then(user => res.json(user))
+    // .catch(err=>{console.log(err)
+    //     res.json({error: "This credential is already registered with us, please enter a diffrent value", message: err.message})
+    // })
 
     // res.send(req.body)//res send krr dia 
     // user.save()
